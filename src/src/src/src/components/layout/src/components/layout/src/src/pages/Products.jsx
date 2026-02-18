afb31595a5c4384ca156fee8385e0d1318957296
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:19:40 +0100

    Create Products.jsx

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">منتجاتنا</h2>
+      <p>هنا قائمة المنتجات الإلكترونية المتاحة.</p>
+    </div>
+  );
+}
+
+export default Products;
