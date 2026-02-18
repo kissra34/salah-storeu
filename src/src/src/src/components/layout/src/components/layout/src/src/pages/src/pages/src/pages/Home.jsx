d747cce18874b8d2788368e8e778cde0d8aa2afd
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:20:51 +0100

    Add Home component with welcome message in Arabic

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/src/pages/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">الصفحة الرئيسية</h2>
+      <p>مرحباً بك في متجر الإلكترونيات، اكتشف أفضل المنتجات!</p>
+    </div>
+  );
+}
+
+export default Home;
