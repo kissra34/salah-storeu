7e99c7a19311e986765cc5d6a707fa1681bb4b51
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:21:34 +0100

    Add About component with store description

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/src/pages/src/pages/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">من نحن</h2>
+      <p>هذا المتجر متخصص في بيع الإلكترونيات بأفضل الأسعار.</p>
+    </div>
+  );
+}
+
+export default About;
