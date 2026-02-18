4ed537b0dbbcdd432d5db178aea7d102f5a6321c
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:26:06 +0100

    Add Button component for UI

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/src/src/src/components/ui/src/components/ui/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Button({ children, onClick }) {
+  return (
+    <button
+      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
