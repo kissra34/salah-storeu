ceed97812db031bd026dfda6b1d37c98af33467a
saalah <salahwldlhouma@gmail.com>
2026-02-18 17:46:29 +0100

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Checkout from './pages/Checkout';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
