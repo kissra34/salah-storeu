7e6a24ad14d8a6b32bbccc17e20474685dd67113
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:16:44 +0100

    Create Header.jsx

---
+++ src/src/src/src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">متجر الإلكترونيات</h1>
+    </header>
+  );
+}
+
+export default Header;
