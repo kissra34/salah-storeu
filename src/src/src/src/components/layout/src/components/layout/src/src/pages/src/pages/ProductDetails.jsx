262f3194b44d4a3ec4c4ece5f61b36a6262040da
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:20:12 +0100

    Add ProductDetails component for product info display

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/src/pages/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductDetails() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">تفاصيل المنتج</h2>
+      <p>هنا ستجد تفاصيل كل منتج إلكتروني.</p>
+    </div>
+  );
+}
+
+export default ProductDetails;
