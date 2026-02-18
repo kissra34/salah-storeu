d46028c41557295c349ac34f044e3ebde5ca6c7e
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:22:38 +0100

    Create Checkout.jsx

---
+++ src/src/src/src/components/layout/src/components/layout/src/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">الدفع</h2>
+      <p>هنا يمكنك مراجعة مشترياتك وإتمام عملية الدفع.</p>
+    </div>
+  );
+}
+
+export default Checkout;
