3ac70069be9961f6aeda086909453893947f7a92
saalah <salahwldlhouma@gmail.com>
2026-02-18 16:08:16 +0100

    Add UserNotRegisteredError component

---
+++ src/components/layout/src/components/layout/src/components/layout/src/components/layout/UserNotRegisteredError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserNotRegisteredError() {
+  return (
+    <div className="p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+      يجب عليك التسجيل لتتمكن من الوصول إلى هذه الصفحة.
+    </div>
+  );
+}
+
+export default UserNotRegisteredError;
