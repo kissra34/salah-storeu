60c0947f76b49a0b509e1ce083971dce166a0a28
saalah <salahwldlhouma@gmail.com>
2026-02-18 15:17:15 +0100

    Create Footer.jsx

---
+++ src/src/src/src/components/layout/src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8 text-center">
+      جميع الحقوق محفوظة © 2026 متجر الإلكترونيات
+    </footer>
+  );
+}
+
+export default Footer;
